719d9f87ab8b5387702de1ea886c6e33f1452560
mo laptop <info@norant.be>
2017-07-04 11:56:16 +0200

    added formfunctionality for dll acces profile

--- src/app/fileview/filetypes/dll-config/dll-config.component.ts
+++ src/app/fileview/filetypes/dll-config/dll-config.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-dll-config',
   templateUrl: './dll-config.component.html',
@@ -7,16 +6,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DLLConfigComponent implements OnInit {
 filedata
-model;
+NOTIMPLEMENTED = "NOT IMPLEMENTED"
+
+
+
 
   constructor() { }
 
   ngOnInit() {
     console.log(this.filedata);
   }
-  onSubmit() {
-    this.model.ActiveAccesClass = "test"
-    console.log(this.model)
+  onSubmit(event) {
+
+    console.log(event)
+
   }
 
 }
